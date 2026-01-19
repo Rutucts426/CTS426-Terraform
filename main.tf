eb3ce1f4ea920c93a7d4f51eb03e95da386664fe
= <=>
2026-01-19 07:19:16 +0000

    Terraform IaC lab - initial

---
+++ main.tf
@@ -0,0 +1,28 @@
+# Creates: Resource Group + Storage Account + Container
+
+resource "azurerm_resource_group" "rg" {
+  name     = var.rg_name
+  location = var.location
+
+  tags = {
+    env = var.env
+  }
+}
+
+resource "azurerm_storage_account" "sa" {
+  name                     = var.storage_account_name
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  tags = {
+    env = var.env
+  }
+}
+
+resource "azurerm_storage_container" "container" {
+  name                  = var.container_name
+  storage_account_name  = azurerm_storage_account.sa.name
+  container_access_type = "private"
+}
